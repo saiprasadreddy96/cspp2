e7365f8bff45ffb5376b0d1b2777b8c83b2c8494
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-27 23:53:43 +0530

    Fahrenheit to celsieus

---
+++ m1/Assignment-3/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double degreesCelsius = scan.nextDouble();
+		System.out.println(degreesCelsius * (1.8) + 32);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
