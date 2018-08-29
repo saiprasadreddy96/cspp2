729dc040fcf0dc560d54521df6c48531414d26d5
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-29 14:03:05 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Solution.java
+++ m3/Assignment-2/Solution.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 /*
 	Do not modify this main function.
 	*/
-public class Solution {
+class Solution {
 /* Fill the main function to print the number of 7's between 1 to n*/
     public static void main(final String[] args) {
 
         Scanner s = new Scanner(System.in);      
         int n = s.nextInt();
         int j, count = 0, a = 7, b = 1, c = 10;
-        for (int i = 1;i <= n;i++) {
+        for (int i = 1; i <= n; i++) {
         	j = i;
         	while (j >= b) {
         		if (j % c == a) {
@@ -23,3 +23,4 @@ public class Solution {
         System.out.print(count);
     }
 }
+
