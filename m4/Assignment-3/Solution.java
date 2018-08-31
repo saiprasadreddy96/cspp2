5ef81e74e45c67f1b5aa66f6397f805e96a9ead5
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-08-31 15:28:13 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 /**.
  * Class for solution.
  */
-public class Solution {/*
-	Do not modify this main function.
+public final class Solution {/*
+    Do not modify this main function.
 	*/
 	/**.
 	 * Constructs the object.
@@ -31,8 +31,7 @@ public class Solution {/*
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(final String[] args)
-	{
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
