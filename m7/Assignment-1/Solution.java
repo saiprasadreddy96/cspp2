9dcb1559c71fba91aa50a10d8179803d458b69f5
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-09-03 19:22:57 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -42,7 +42,13 @@ public class Solution {
 	public static void main(final String args[]) {
     	Scanner s = new Scanner(System.in);
     	String input = s.next();
+        /**.
+    	 * { var_description }
+    	 */
     	InputValidator i = new InputValidator(input);    	
     	System.out.println(i.validateData());
     }
 }
+
+
+
