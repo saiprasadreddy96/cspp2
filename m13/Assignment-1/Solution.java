8f2cb2ad1553055b7738b198ee352d6a097890e4
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-09-08 11:37:41 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m13/Assignment-1/Solution.java
+++ m13/Assignment-1/Solution.java
@@ -1,26 +1,45 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
-
 /**
  * Class for set.
  * @author : 
+ */
+/**.
+ * Class for set.
  */
 class Set {
     //your code goes here...
     //Good luck :-)
     private int[] set;
     private int size, resize;
+    private static int m = 10;
+    /**.
+     * Constructs the object.
+     *
+     * @param      capacity  The capacity
+     */
     public Set(int capacity) {
     	set = new int[capacity];
     	size = 0;
-    	resize = 10;
+    	resize = m;
     }
-
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int size() {
     	return size;
     }
-    public boolean contains(int item) {
+    /**.
+     * { function_description }
+     *
+     * @param      item  The item
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean contains(final int item) {
     	for (int i = 0; i < size; i++) {
     		if (set[i] == item) {
     			return true;
@@ -28,6 +47,11 @@ class Set {
     	}
     	return false;
     }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
     	if (size == 0) {
     		return "{}";
@@ -40,7 +64,12 @@ class Set {
     	str += set[i] + "}";
     	return str;
      }
-     public void add(int item) {
+     /**.
+      * { function_description }
+      *
+      * @param      item  The item
+      */
+     public void add(final int item) {
      	if (!contains(item)) {
      		if (size == resize) {
      			resize();
@@ -48,19 +77,34 @@ class Set {
      		set[size++] = item;
      	}
      }
+     /**.
+      * { function_description }
+      */
      public void resize() {
      	resize = 2 * size;
      	int[] set1 = new int[resize];
      	System.arraycopy(set, 0, set1, 0, set.length);
      	set = set1;
      }
-     public void add(int[] items) {
+     /**.
+      * { function_description }
+      *
+      * @param      items  The items
+      */
+     public void add(final int[] items) {
      	for (int i = 0; i < items.length; i++) {
      		add(items[i]);
      	} 
      }
-     public Set intersection(Set other) {
-     	Set is = new Set(10);
+     /**.
+      * { function_description }
+      *
+      * @param      other  The other
+      *
+      * @return     { description_of_the_return_value }
+      */
+     public Set intersection(final Set other) {
+     	Set is = new Set(m);
      	for (int i = 0; i < size; i++) {
      		if (other.contains(this.set[i])) {
      			is.add(this.set[i]);
@@ -68,7 +112,14 @@ class Set {
      	}
      	return is;
      }
-     public int[][] cartesianProduct(Set other) {
+     /**.
+      * { function_description }
+      *
+      * @param      other  The other
+      *
+      * @return     { description_of_the_return_value }
+      */
+     public int[][] cartesianProduct(final Set other) {
      	int r = this.size * other.size, c = 2, k = 0;
      	int[][] a = new int[r][c];
      	for (int i = 0; i <this.size; i++) {
@@ -79,15 +130,20 @@ class Set {
      		}
      	}
      	return a;
-
      }
-     public Set retainAll(int[] items) {
-     	Set other = new Set(10);
+     /**.
+      * { function_description }
+      *
+      * @param      items  The items
+      *
+      * @return     { description_of_the_return_value }
+      */
+     public Set retainAll(final int[] items) {
+     	Set other = new Set(m);
      	other.add(items);
      	return this.intersection(other);
      }
-
-}
+ }
 /**
  * Solution class for code-eval.
  */
@@ -95,6 +151,10 @@ public final class Solution {
     /**
      * Constructs the object.
      */
+    /**.
+     * Constructs the object.
+     */
+    private static int m =10;
     private Solution() {
 
     }
@@ -104,6 +164,13 @@ public final class Solution {
      * @param      s     { string input from test case file }
      *
      * @return     { int array from the given string }
+     */
+    /**.
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
      */
     public static int[] intArray(final String s) {
         String input = s;
@@ -122,9 +189,14 @@ public final class Solution {
      *
      * @param      args  The arguments
      */
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         // instantiate this set
-        Set s = new Set(10);
+        Set s = new Set(m);
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -153,8 +225,8 @@ public final class Solution {
                 }
                 break;
                 case "intersection":
-                s = new Set(10);
-                Set t = new Set(10);
+                s = new Set(m);
+                Set t = new Set(m);
                 intArray = intArray(tokens[1]);
                 s.add(intArray);
                 intArray = intArray(tokens[2]);
@@ -162,15 +234,15 @@ public final class Solution {
                 System.out.println(s.intersection(t));
                 break;
                 case "retainAll":
-                s = new Set(10);
+                s = new Set(m);
                 intArray = intArray(tokens[1]);
                 s.add(intArray);
                 intArray = intArray(tokens[2]);
                 System.out.println(s.retainAll(intArray));
                 break;
                 case "cartesianProduct":
-                s = new Set(10);
-                t = new Set(10);
+                s = new Set(m);
+                t = new Set(m);
                 intArray = intArray(tokens[1]);
                 s.add(intArray);
                 intArray = intArray(tokens[2]);
