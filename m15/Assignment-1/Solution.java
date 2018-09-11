59174ec60649a685207bc91f815a9bc812d0bba2
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-09-11 19:34:24 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- m15/Assignment-1/Solution.java
+++ m15/Assignment-1/Solution.java
@@ -315,7 +315,7 @@ final class List {
         if (this.size == list1.size) {
             for (int i = 0; i < this.size; i++) {
                 int count = 0;
-                for (int j = 0; j < this.size; j++) {
+                for (int j = 0; j < list1.size; j++) {
                     if (this.list[i] == list1.list[j]) {
                         count++;
                     }
