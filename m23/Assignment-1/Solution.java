edb04b5e543c2b11a1c6bd35f7df2d74a134a529
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-09-20 17:52:29 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- m23/Assignment-1/Solution.java
+++ m23/Assignment-1/Solution.java
@@ -19,6 +19,7 @@ class Document {
 		System.out.println("buffer");
 		System.out.println(foldername + " " + filename);
 		FileInputStream fis = new FileInputStream("F:\\recyle bin\\MSIT\\cspp2\\m23\\Assignment-1\\foldername\\filename");
+		System.out.println("buffer1");
 		InputStreamReader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 		System.out.println("buffer1");
